39090549a940ec80209208faceb1f9fad8f98f06
zhanghanduo <hzhang032@e.ntu.edu.sg>
2018-08-08 10:34:20 +0800

    :memo: add test script

---
+++ test/params.py
@@ -0,0 +1,21 @@
+TRAINING_PARAMS = \
+{
+    "model_params": {
+        "backbone_name": "darknet_53",
+        "backbone_pretrained": "",
+    },
+    "yolo": {
+        "anchors": [[[116, 90], [156, 198], [373, 326]],
+                    [[30, 61], [62, 45], [59, 119]],
+                    [[10, 13], [16, 30], [33, 23]]],
+        "classes": 80,
+    },
+    "batch_size": 16,
+    "iou_thres": 0.5,
+    "val_path": "../data/coco/5k.txt",
+    "annotation_path": "../data/coco/annotations/instances_val2014.json",
+    "img_h": 416,
+    "img_w": 416,
+    "parallels": [0],
+    "pretrain_snapshot": "../weights/yolov3_weights_pytorch.pth",
+}
